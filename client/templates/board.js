e47c54450ba1b5ddac1ce7251122dc4fa4344ae1
Thibault Lenclos <tlenclos@jolicode.com>
2014-11-28 13:56:02 +0100

    Minor improvement on subscriptions

--- client/templates/board.js
+++ client/templates/board.js
@@ -1,9 +1,8 @@
 Template.board.rendered = function() {
     Meteor.subscribe('pixels', this.data._id);
-    var user = Meteor.users.findOne({_id: this.data.userId});
 
     new Pixelboard(
         this.data._id,
-        user ? user._id : null
+        this.data.userId
     ).setup(); // TODO How to clear previous objects created while we navigate to boards ?
 };
--- server/server.js
+++ server/server.js
@@ -12,21 +12,29 @@ Meteor.publish("pixels", function(boardId) {
     this.unblock();
     return PixelsCollection.find({boardId: boardId});
 });
-Meteor.publish("boards", function() {
+Meteor.publish("boards", function(limit) {
     this.unblock();
-    return BoardsCollections.find({}, {sort: {createdAt: -1}});
+    limit = limit || 10;
+
+    var options = {
+        sort: {createdAt: -1},
+        limit: limit
+    };
+
+    return BoardsCollections.find({}, options);
 });
 Meteor.publish("board", function(boardId) { // TODO Merge the user in this subscription
     this.unblock();
     return BoardsCollections.find({_id: boardId});
 });
-Meteor.publish("boardUser", function(boardId) {
-    var board = BoardsCollections.findOne({_id: boardId});
-    return Meteor.users.find({_id: board.userId}, {fields: {_id: 1, profile: 1}});
+Meteor.publish("boardOwner", function(userId) {
+    this.unblock();
+    return Meteor.users.find({_id: userId}, {fields: {_id: 1, profile: 1}});
 });
 
 Meteor.publish('onlineUsers', function() {
     this.unblock();
+
     return [
         Meteor.users.find({
             "status.online": true
@@ -44,8 +52,7 @@ Meteor.publish('onlineUsers', function() {
 Meteor.startup(function(){
     // Mongo indexes
     console.log('Ensure index mongo DB');
-    PixelsCollection._ensureIndex({ boardId:1}, {dropDups: true});
-    PixelsCollection._ensureIndex({x:1, y:1, boardId:1}, {unique: 1, dropDups: true});
+    BoardsCollections._ensureIndex({createdAt:1});
 
     // Configure oauth accounts
     var configureService = function(name, config) {
